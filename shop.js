c39f621b11069cae78a88c48ba95b4f244fccbcf
kevinsequeira2 <kevinsequeira758@gmail.com>
2020-07-26 11:24:42 -0600

    adding product a the list

---
+++ shop.js
@@ -0,0 +1,41 @@
+let list=[];
+
+
+function add(id) {
+    
+    if(id=="blueShirt"){
+        list.push("blueShirt");
+        for (let i = 0; i < list.length; i++) {
+            const element = list[i];
+            alert(element);
+        
+    }
+    }
+    if (id=="blackShirt") {
+        list.push("blackShirt");
+        for (let i = 0; i < list.length; i++) {
+            const element = list[i];
+            alert(element);
+    }
+    
+    
+    }
+    if (id=="purpleShirt") {
+        list.push("purpleShirt");
+        for (let i = 0; i < list.length; i++) {
+            const element = list[i];
+            alert(element);
+    }
+    
+    
+    }
+    if (id=="whiteShirt") {
+        list.push("whiteShirt");
+        for (let i = 0; i < list.length; i++) {
+            const element = list[i];
+            alert(element);
+    }
+    
+    
+    }
+}
